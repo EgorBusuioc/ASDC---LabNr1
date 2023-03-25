09fea83a1b53bc4060fe971b0b79cb6b2ab9c6db
EgorBusuioc <busuiocegor@gmail.com>
2023-03-25 19:23:26 +0200

    fibonacciSearch

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Student student;
+    Node left;
+    Node right;
+
+    public Node(Student student) {
+        this.student = student;
+        left = null;
+        right = null;
+    }
+}
